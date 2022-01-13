f563795a78cf8bcf52fdd6bbd8c01522038b2024
Victor Ferrando <victor.ferrando@hotmail.com>
2022-01-13 15:32:41 -0300

    Initial Commit

---
+++ src/game/br/com/viittor/entities/ClassType.java
@@ -0,0 +1,10 @@
+package game.br.com.viittor.entities;
+
+public class ClassType {
+	private int health;
+	private int defensePoints;
+	private int attackPoints;
+	private int damage;
+	
+
+}
---
+++ src/game/br/com/viittor/entities/Player.java
@@ -0,0 +1,9 @@
+package game.br.com.viittor.entities;
+
+public class Player {
+	private String name;
+	private int gender;
+	private String classType;
+	private String weapon;
+
+}
